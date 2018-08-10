2f6ed387ffdca1ecde9d246aee4398c68a8364fe
Nesmikkuma <Nesmith05@yahoo.com>
2018-08-10 18:38:14 -0400

    added on-click event

---
+++ Assets/Javascript/train.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+    var config = {
+        apiKey: "AIzaSyDl3XCHlx2IOKkZIXEGZ3k9mJ6R5QB7HYk",
+        authDomain: "train-schedule-3e122.firebaseapp.com",
+        databaseURL: "https://train-schedule-3e122.firebaseio.com",
+        projectId: "train-schedule-3e122",
+        storageBucket: "train-schedule-3e122.appspot.com",
+        messagingSenderId: "125850147682"
+    };
+    firebase.initializeApp(config);
+
+        var database = firebase.database();
+
+        var clickCounter = 0;
+
+        $("#submit").on("click", function() {
+         //Take value from the input forms
+         var trainName = $("#train-name").val().trim();
+         var destination = $("#destination").val().trim();
+         var time = $("#time").val().trim();
+         var frequency = $("#frequency").val().trim();
+
+         //Push to firebase
+         database.ref().push ({
+             trainName: trainName,
+             destination: destination,
+             time: time,
+             frequency: frequency
+            });
+        var newRow = $("<tr>").append(trainName);
+        });
+})
